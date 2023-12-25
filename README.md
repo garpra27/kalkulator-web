1654fe947ac27f749371f253bad894591ea1fb96
garpra27 <gilangapradana01@gmail.com>
2023-12-25 20:34:59 +0700

    push project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Calculator Web
+
+Ini adalah repository yang berisi proyek web sederhana yang menyediakan kalkulator online yang dapat digunakan untuk melakukan operasi matematika dasar. Proyek ini dibangun menggunakan HTML, CSS, dan JavaScript, dan merupakan alat praktis untuk melakukan perhitungan cepat dan mudah di browser web Anda.
+
+##### Teknologi
+
+- HTML
+- CSS
+- JavaScript
---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+//Membuat DOM Selector untuk Element HTML
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+
+//Variable
+let out = "";
+let cekMin = true;
+let cekOperator;
+let hasilHitung = null;
+
+//Memberi Event
+input.addEventListener("click", function (e) {
+  if (e.target.className == "angka") {
+    out += e.target.textContent;
+    cekMin = false;
+    hasilHitung = null;
+  } else if (e.target.textContent != "-" && cekMin == true && out == "") {
+    out = "";
+  } else if (e.target.className == "operator") {
+    const cek = /\d/;
+
+    if (out.length > 1) {
+      cekOperator = out.substr(out.length - 1);
+    }
+
+    if (e.target.textContent == "C") {
+      out = "";
+      cekMin = true;
+    } else if (e.target.textContent == "DEL" && hasilHitung == null) {
+      out = out.slice(0, -1);
+    } else if (e.target.textContent == "DEL" && hasilHitung != null) {
+      out = "";
+      hasilHitung = null;
+      cekMin = true;
+    } else {
+      if (e.target.textContent != "-" && cekMin == true) {
+        out = "";
+      } else {
+        if (cek.test(cekOperator)) {
+          out += e.target.textContent;
+        } else {
+          let temp = out.split("");
+          temp.pop();
+          temp.push(e.target.textContent);
+          let hasil = temp.join("");
+          out = hasil;
+        }
+      }
+    }
+  } else if (e.target.className == "hasil") {
+    hasilHitung = eval(out);
+    out = hasilHitung;
+    cekMin = false;
+  }
+
+  output.textContent = out;
+});
